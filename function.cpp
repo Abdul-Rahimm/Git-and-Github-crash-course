bf91885ecba069c3fac5f5676fa13414b8720e6a
Abdul-Rahimm <k213951@nu.edu.pk>
2023-11-24 23:04:21 +0500

    new cpp file containing 2x function

---
+++ function.cpp
@@ -0,0 +1,21 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) x.begin(), x.end()
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+ 
+
+void algorithm(int &x){
+    x = x*2;
+}
+
+int main(){
+    int x = 10;        //int variable   
+
+    algorithm(x);       //pased that variable to a function
+
+    cout << x << endl; //print x
+
+
+}
