e2444b79d1fd65978e37720d42d7b84c31af6ad0
Abdul-Rahimm <k213951@nu.edu.pk>
2023-11-25 02:02:15 +0500

    added function addtwo

--- function.cpp
+++ function.cpp
@@ -9,13 +9,15 @@ typedef vector<vi> vvi;
 void algorithm(int &x){
     x = x*2;
 }
+void addtwo(int &x){
+    x += 2;
+}
 
 int main(){
     int x = 10;        //int variable   
 
     algorithm(x);       //pased that variable to a function
+    addtwo(x);
 
     cout << x << endl; //print x
-
-
 }
